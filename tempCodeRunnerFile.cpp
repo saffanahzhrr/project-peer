406619c871189a745475dcb2982265bde8a61b2e
saffanahzhrr <saffanahazzahra1517@gmail.com>
2025-10-11 14:01:56 +0700

    update 2

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+bool isPrima(int n) {
+//     if (n < 2) {
+//         return false;
+//     }
+//     for (int i = 2; i < n; i++) {
+//         if (n % i == 0) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
